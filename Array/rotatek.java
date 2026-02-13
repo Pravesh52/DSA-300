4f109ea2c1d840938b5033c17af4609e96f67451
PRAVESH TIWARI <pravesht252@gmail.com>
2026-02-13 21:19:47 +0530

    DSA

---
+++ Array/rotatek.java
@@ -0,0 +1,33 @@
+//Leetcode probelm 189. Rotate Array
+
+public class rotatek {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7};
+        int k=3;
+
+        int n=arr.length;
+
+        k=k%n;
+        
+        for(int i=0;i<k;i++){
+
+            int lastelement=arr[n-1];
+
+            for(int j=n-1;j>0;j--){
+                arr[j]=arr[j-1];
+            }
+            
+            arr[0]=lastelement;
+           
+
+        }
+
+        for(int num: arr){
+            System.out.println(num+" ");
+        }
+
+
+
+
+    }
+}
